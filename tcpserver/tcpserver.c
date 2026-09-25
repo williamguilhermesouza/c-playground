194d5de0f60255ae72ba3e4bf653584d7574f78c
williamguilhermesouza <williamguilhermesouza@gmail.com>
2026-09-25 08:40:18 -0300

    fixed buffer on recv

--- tcpserver/tcpserver.c
+++ tcpserver/tcpserver.c
@@ -16,10 +16,9 @@ struct tcp_server
 {
 	char *port;
 	int sockfd;
-    int accepted_fd;
-    server_state state;
+	int accepted_fd;
+	server_state state;
 };
-
 
 // struct message
 // {
@@ -82,7 +81,7 @@ int create_server(struct tcp_server *sv)
 
 	struct sockaddr_storage their_addr;
 	socklen_t sin_size;
-    // improve this with fork
+	// improve this with fork
 	int new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
 	if (new_fd < 0)
 	{
@@ -98,85 +97,87 @@ int create_server(struct tcp_server *sv)
 	fprintf(stdout, "Connected to %s\n", s);
 
 	sv->sockfd = sockfd;
-    sv->accepted_fd = new_fd;
+	sv->accepted_fd = new_fd;
 
 	return 0;
 }
 
-void recv_loop(struct tcp_server *server, int (*on_message_recv)(char *, size_t))
+void recv_loop(struct tcp_server *server,
+			   int (*on_message_recv)(char *, size_t))
 {
-    int buffer_size = 1024;
-    char *buffer = calloc(buffer_size, sizeof(char));
+	int buffer_size = 1024;
+	char *buffer = calloc(buffer_size, sizeof(char));
 
-    int bytes_rcv = 0;
-    int total_bytes = 0;
-    while(1)
-    {
-        // buffer filled, realoc
-        if (total_bytes == buffer_size)
-        {
-            size_t prev_buffer_size = buffer_size;
-            buffer_size *= 2;
-            char *p = realloc(buffer, buffer_size);
-            if (p == NULL)
-            {
-                perror("realloc");
-                break;
-            }
+	int bytes_rcv = 0;
+	int total_bytes = 0;
+	while (1)
+	{
+		// buffer filled, realoc
+		if (total_bytes == buffer_size)
+		{
+			size_t prev_buffer_size = buffer_size;
+			buffer_size *= 2;
+			char *p = realloc(buffer, buffer_size);
+			if (p == NULL)
+			{
+				perror("realloc");
+				break;
+			}
 
-            memset(p + prev_buffer_size, 0, buffer_size - prev_buffer_size); 
-            buffer = p;
-        }
+			memset(p + prev_buffer_size, 0, buffer_size - prev_buffer_size);
+			buffer = p;
+		}
 
-        bytes_rcv = recv(server->accepted_fd, buffer, buffer_size, 0);
-        if (bytes_rcv == -1)
-        {
-            perror("recv");
-            break;
-        }
-        if (bytes_rcv == 0)
-        {
-            fprintf(stdout, "Received empty msg. Client closed \n");
-            break;
-        }
+		bytes_rcv = recv(server->accepted_fd, buffer + total_bytes,
+						 buffer_size - total_bytes, 0);
+		if (bytes_rcv == -1)
+		{
+			perror("recv");
+			break;
+		}
+		if (bytes_rcv == 0)
+		{
+			fprintf(stdout, "Received empty msg. Client closed \n");
+			break;
+		}
 
-        total_bytes += bytes_rcv;
+		total_bytes += bytes_rcv;
 
-        //TODO create on_message_recv arg and a http func of this
-        int bytes_processed = on_message_recv(buffer, total_bytes);
-        if (bytes_processed == -1)
-        {
-            fprintf(stderr, "Failed processing received message");
-            break;
-        }
+		// TODO create on_message_recv arg and a http func of this
+		int bytes_processed = on_message_recv(buffer, total_bytes);
+		if (bytes_processed == -1)
+		{
+			fprintf(stderr, "Failed processing received message");
+			break;
+		}
 
-        // get the not consumed portion of the buffer and 
-        // move it to the beginning
-        char *not_consumed_ptr = buffer + bytes_processed;
-        size_t not_consumed_size = buffer_size - bytes_processed;
-        memmove(buffer, not_consumed_ptr, not_consumed_size);
+		// get the not consumed portion of the buffer and
+		// move it to the beginning
+		char *not_consumed_ptr = buffer + bytes_processed;
+		size_t not_consumed_size = total_bytes - bytes_processed;
+		memmove(buffer, not_consumed_ptr, not_consumed_size);
 
-        // zero out any garbage data after moving the buffer
-        size_t empty_buffer_size = buffer_size - not_consumed_size;
-        char *not_consumed_end = buffer + not_consumed_size;
-        memset(not_consumed_end, 0, empty_buffer_size);
+		// zero out any garbage data after moving the buffer
+		size_t empty_buffer_size = buffer_size - not_consumed_size;
+		char *not_consumed_end = buffer + not_consumed_size;
+		memset(not_consumed_end, 0, empty_buffer_size);
 
-        total_bytes -= bytes_processed;
-    }
+		total_bytes -= bytes_processed;
+	}
 
-    free(buffer);
+	free(buffer);
 }
 
 void close_server(struct tcp_server *server)
 {
-    close(server->accepted_fd);
-    close(server->sockfd);
+	close(server->accepted_fd);
+	close(server->sockfd);
 }
 
 int r_msg(char *buf, size_t size)
 {
-    printf("%s\n", buf);
-    return size;
+	printf("%s\n", buf);
+	return size;
 }
 
 // create a tcp server capable of knowing how to join message chunks into a
@@ -184,14 +185,14 @@ int r_msg(char *buf, size_t size)
 // complete) will be given from the protocol that is built on the server
 int main(void)
 {
-    printf("Starting server at port 3333\n");
+	printf("Starting server at port 3333\n");
 
-    struct tcp_server sv;
-    sv.port = "3333";
+	struct tcp_server sv;
+	sv.port = "3333";
 
-    // in the future, test with stdin and out file descriptors
-    create_server(&sv);
-    recv_loop(&sv, r_msg);
-    close_server(&sv);
-    printf("Server shutting down...\n");
+	// in the future, test with stdin and out file descriptors
+	create_server(&sv);
+	recv_loop(&sv, r_msg);
+	close_server(&sv);
+	printf("Server shutting down...\n");
 }
